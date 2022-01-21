e8f4597b3631507b2c8281b1b225bc2a458e7d2b
c00271288 <c00271288@itcarlow.ie>
2022-01-21 13:26:44 +0000

    First Square

--- Simon_Game/Simon_Game/Game.cpp
+++ Simon_Game/Simon_Game/Game.cpp
@@ -1,8 +1,6 @@
 /// <summary>
-/// @author Peter Lowe
-/// @date May 2019
-///
-/// you need to change the above lines or lose marks
+/// @author Scott Mitchell
+/// @date January 2022
 /// </summary>
 
 #include "Game.h"
@@ -21,7 +19,7 @@ Game::Game() :
 	m_exitGame{false} //when true game will exit
 {
 	setupFontAndText(); // load font 
-	setupSprite(); // load texture
+	setUpButtons();
 }
 
 /// <summary>
@@ -110,9 +108,8 @@ void Game::update(sf::Time t_deltaTime)
 /// </summary>
 void Game::render()
 {
-	m_window.clear(sf::Color::White);
-	m_window.draw(m_welcomeMessage);
-	m_window.draw(m_logoSprite);
+	m_window.clear();
+	m_window.draw(m_redSquare);
 	m_window.display();
 }
 
@@ -121,31 +118,12 @@ void Game::render()
 /// </summary>
 void Game::setupFontAndText()
 {
-	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
-	{
-		std::cout << "problem loading arial black font" << std::endl;
-	}
-	m_welcomeMessage.setFont(m_ArialBlackfont);
-	m_welcomeMessage.setString("SFML Game");
-	m_welcomeMessage.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
-	m_welcomeMessage.setPosition(40.0f, 40.0f);
-	m_welcomeMessage.setCharacterSize(80U);
-	m_welcomeMessage.setOutlineColor(sf::Color::Red);
-	m_welcomeMessage.setFillColor(sf::Color::Black);
-	m_welcomeMessage.setOutlineThickness(3.0f);
-
+	
 }
 
-/// <summary>
-/// load the texture and setup the sprite for the logo
-/// </summary>
-void Game::setupSprite()
+void Game::setUpButtons()
 {
-	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
-	{
-		// simple error message if previous call fails
-		std::cout << "problem loading logo" << std::endl;
-	}
-	m_logoSprite.setTexture(m_logoTexture);
-	m_logoSprite.setPosition(300.0f, 180.0f);
+	m_redSquare.setFillColor(sf::Color::Red);
+	m_redSquare.setSize(sf::Vector2f(200, 200));
+	m_redSquare.setPosition(300.0f, 90.0f);
 }
--- Simon_Game/Simon_Game/Game.h
+++ Simon_Game/Simon_Game/Game.h
@@ -1,6 +1,5 @@
 /// <summary>
-/// author Pete Lowe May 2019
-/// you need to change the above line or lose marks
+/// author Scott Mitchell January 2022
 /// </summary>
 #ifndef GAME_HPP
 #define GAME_HPP
@@ -29,14 +28,18 @@ private:
 	void render();
 	
 	void setupFontAndText();
-	void setupSprite();
+	void setUpButtons();
 
 	sf::RenderWindow m_window; // main SFML window
+
+	sf::Color RED{ 180, 0, 0, 225 };
+
 	sf::Font m_ArialBlackfont; // font used by message
 	sf::Text m_welcomeMessage; // text used for message on screen
-	sf::Texture m_logoTexture; // texture used for sfml logo
-	sf::Sprite m_logoSprite; // sprite used for sfml logo
-	bool m_exitGame; // control exiting game
+	
+	sf::RectangleShape m_redSquare;
+
+	bool m_exitGame; // control exiting game 
 
 };
 
